d1a2682eac959c2cf4e7955a89fe416ae94b1f80
Rimyo <mingjie.zhang.pro@gmail.com>
2023-11-09 17:55:08 +0100

    Home page v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -34,16 +34,16 @@ export default App*/
 
 function Login() {
   const [isLogged, setLogging] = useState(false)
-  if(!isLogged)
-  return (
-    <div className="grid bg-zinc-800 rounded-lg place-self-center">
-      <img src='./images/bowser.webp' className="w-32 py-2 bg-contain h-auto place-self-center" />
-      <p className="text-gray-100 px-4 " >Hey pas si vite ! Vous n'êtes pas connecté !</p>
-      <button onClick={() => setLogging(true)} className="bg-gray-300 text-black mx-20 my-4 py-1">Se connecter</button>
-    </div>
-  )
-  else{
-    //Home
+  if (!isLogged)
+    return (
+      <div className="grid bg-zinc-800 rounded-lg place-self-center">
+        <img src='./images/bowser.webp' className="w-32 py-2 bg-contain h-auto place-self-center" />
+        <p className="text-gray-100 px-4 " >Hey pas si vite ! Vous n'êtes pas connecté !</p>
+        <button onClick={() => setLogging(true)} className="bg-gray-300 text-black mx-20 my-4 py-1">Se connecter</button>
+      </div>
+    )
+  else {
+    return <Home />
   }
 
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,8 @@
 function Home() {
+    return (
+        <div className="place-content-center">
+            <h1>Home</h1>
+        </div>
+    )
 }
 export default Home
